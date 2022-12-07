f3550e78b294800b271102da00c83e42891f4262
Douglas Barbosa <douglas.barbosa@sensedia.com>
2022-12-07 19:14:25 -0300

    code without Open Closed Principle

---
+++ src/main/java/com/github/Dagurasu56/solid/open-closed-principle/HealthInsuranceCustomerProfile.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class HealthInsuranceCustomerProfile {
+
+    public boolean isLoyalCustomer() {
+        return new Random().nextBoolean();
+    }
+}
---
+++ src/main/java/com/github/Dagurasu56/solid/open-closed-principle/VehicleInsuranceCustomerProfile.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class VehicleInsuranceCustomerProfile {
+
+    public boolean isLoyalCustomer() {
+        return new Random().nextBoolean();
+    }
+}
